cc0710e1cee29102f45f286a92dcdc508e11efa6
Mike Rode <mike.rode@malauzai.com>
2019-09-22 20:50:40 -0400

    Switch scripts from gitlab repos to azure repos

--- scripts/test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-ssh -t preproduction.malauzai.com "sudo su - mastermonkey -c \"ls /home/mastermonkey/statement-fetching-service/statements/*service \""
-ssh -t preproduction.malauzai.com "sudo su - mastermonkey -c \"rm -rf /home/mastermonkey/statement-fetching-service/statements/*service \""
